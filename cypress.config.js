38ed1112e7338eac20968e2ba6ae7e1fa59fffc3
Victor Figueiredo <victorfernando.bsi@gmail.com>
2025-06-03 09:41:19 -0300

    Corrindo erro

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
       allureWriter(on, config);
       return config;
     },
