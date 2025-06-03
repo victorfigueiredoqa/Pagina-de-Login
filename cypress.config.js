dd0207293b4ed05c27385087f078b62072da08b8
Victor Figueiredo <victorfernando.bsi@gmail.com>
2025-06-02 22:31:04 -0300

    Incrementação do Allure Report local

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,20 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
 
-    baseUrl: 'https://practicetestautomation.com/practice-test-login/',
-    viewportWidth: 1920,
-    viewportHeight: 1080,
     env: {
+      allureResultsPath: "allure-results",
+      baseUrl: 'https://practicetestautomation.com/practice-test-login/',
+      viewportWidth: 1920,
+      viewportHeight: 1080,
       password: 'Password123'
-    }
+    },
   },
-});
+} );
